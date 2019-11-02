4657bc90a371c83cef5aeced28d8671857e0db78
NtheOtaku <n_studios@outlook.com>
2019-11-02 01:26:17 -0400

    Add files via upload

---
+++ Launch Steam with Classic Library.bat
@@ -0,0 +1,13 @@
+attrib -R "C:\Program Files (x86)\Steam\package\*.*"
+del /q "C:\Program Files (x86)\Steam\package\*.*"
+
+xcopy /s /i /q "C:\Program Files (x86)\Steam\oldpackage" "C:\Program Files (x86)\Steam\package"
+
+attrib +R "C:\Program Files (x86)\Steam\package\steam_client_win32"
+attrib +R "C:\Program Files (x86)\Steam\package\steam_client_win32.manifest"
+
+start C:\"Program Files (x86)"\Steam\Steam.exe
+
+
+
+rem If your Steam folder isn't located on your C: drive, change it to the drive that it's located on.
